88d0277564b550b8478ddfdaba196d07ba11799f
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2024-01-30 02:27:26 +0800

    re-add realtime google translate

--- start-voice-transcriber.py
+++ start-voice-transcriber.py
@@ -22,6 +22,7 @@ import librosa
 import wave
 from scipy.io import wavfile
 from scipy.signal import resample
+from googletrans import Translator, LANGUAGES
 
 WHISPER_MODEL = os.environ.get("WHISPER_MODEL", "small")
 
@@ -44,7 +45,7 @@ FOLLOW_UP_THRESHOLD = 5.0
 def transcribe_worker(sound_queue: mp.Queue):
     RATE = 16000
     whisper_model = whisper.load_model(WHISPER_MODEL)
-
+    translator = Translator()
     with open(OUTPUT_FILENAME, "w") as f:
         while True:
             try:
@@ -54,13 +55,14 @@ def transcribe_worker(sound_queue: mp.Queue):
                 if np.all(np.abs(audio_data) < 0.02):
                     continue
                 
-                start_transcribe = time.time()
+                
                 result = whisper_model.transcribe(audio_data)
 
                 # strip and normalize spaces
                 text = result['text'].strip().replace("  ", " ")
-                f.write(text + "\n")
-                print("text:", text)
+                translated = translator.translate(text, src='ja', dest='en')
+                f.write(translated.text + "\n")
+                print("text:", translated.text)
             except Exception as e:
                 print(e)
             finally:
