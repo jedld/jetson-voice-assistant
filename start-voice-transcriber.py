694409ca79947057a987b6ad5635918e5c5630ce
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2024-01-30 00:54:41 +0800

    Write transcription to file

--- start-voice-transcriber.py
+++ start-voice-transcriber.py
@@ -24,18 +24,11 @@ from scipy.io import wavfile
 from scipy.signal import resample
 
 WHISPER_MODEL = os.environ.get("WHISPER_MODEL", "base.en")
-vocalizer_class = os.environ.get("VOCALIZER_CLASS", "CoquiTTS")
-VOCALIZER_CLASS =  CoquiTTS if vocalizer_class == "CoquiTTS" else CoquiTTSMulti if vocalizer_class == "CoquiTTSMulti" else BarkVocalizer
 
 INPUT_DEVICE_NAME = os.environ.get("INPUT_DEVICE", default="Blue Snowball")
-OUTPUT_DEVICE_NAME = os.environ.get("OUTPUT_DEVICE", default="USB PnP Audio Device")
-OUTPUT_DEVICE_SAMPLE_RATE = int(os.environ.get("OUTPUT_DEVICE_SAMPLE_RATE", default=48000))
-CONTEXT_LENGTH = int(os.environ.get("CONTEXT_LENGTH", default=340))
+OUTPUT_FILENAME = os.environ.get("OUTPUT_FILENAME", default="transcription.txt")
 
-# If using the Yahboom Jetson Cube case with LED lights attached, set this to true
-HAS_YAHBOOM_CASE = os.environ.get("HAS_YAHBOOM_CASE", default="false").lower() == "true"
 MAX_FRAMES = 500
-SYSTEM_MESSAGE = "You are MistralOrca, a large language model trained by Alignment Lab AI. Reply in a way as if you are in a voice conversation with a human. Make your answers short and concise."
 
 # Get device
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -43,8 +36,6 @@ print(device)
 
 ctx = mp.get_context('spawn')
 sound_queue = ctx.JoinableQueue()
-
-
 start_conversation_threshold = 5.0 # Give the user 5 seconds to start a conversation before returning to standby
 silence_threshold = 0.02  # This value might need adjustment
 silence_duration = 2.0  # Duration of silence in seconds before determining if the user is done with his/her sentence
@@ -53,27 +44,30 @@ FOLLOW_UP_THRESHOLD = 5.0
 def transcribe_worker(sound_queue: mp.Queue):
     RATE = 16000
     whisper_model = whisper.load_model(WHISPER_MODEL)
-    while True:
-        try:
-            audio = sound_queue.get()
-            audio_data = nr.reduce_noise(y=audio, sr=RATE, use_torch=True)
-            audio_data = audio_data.astype(np.float32) / 32768.0
-            if np.all(np.abs(audio_data) < 0.02):
-                print("no audio detected")
-                silence_timer = None
-                continue
-            
-            start_transcribe = time.time()
-            result = whisper_model.transcribe(audio_data)
-            print(f"transcribed in {time.time() - start_transcribe} seconds")
+    with open(OUTPUT_FILENAME, "w") as f:
+        while True:
+            try:
+                audio = sound_queue.get()
+                audio_data = nr.reduce_noise(y=audio, sr=RATE, use_torch=True)
+                audio_data = audio_data.astype(np.float32) / 32768.0
+                if np.all(np.abs(audio_data) < 0.02):
+                    print("no audio detected")
+                    silence_timer = None
+                    continue
+                
+                start_transcribe = time.time()
+                result = whisper_model.transcribe(audio_data)
+                print(f"transcribed in {time.time() - start_transcribe} seconds")
 
-            # strip and normalize spaces
-            text = result['text'].strip().replace("  ", " ")
-            print("text:", text)
-        except Exception as e:
-            print(e)
-        finally:
-            sound_queue.task_done()
+                # strip and normalize spaces
+                text = result['text'].strip().replace("  ", " ")
+                f.write(text + "\n")
+                print("text:", text)
+            except Exception as e:
+                print(e)
+            finally:
+                sound_queue.task_done()
+                f.flush()
 
 
 def start_speech_recognizer(sound_queue, audio_device, pyaudio_device_index):
